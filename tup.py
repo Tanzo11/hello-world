85f5505839bef5928454612e244e16bb3d812fc1
Tanzo11 <115859113+Tanzo11@users.noreply.github.com>
2024-02-06 19:27:35 +0530

    Update tup.py

--- tup.py
+++ tup.py
@@ -1,8 +1,7 @@
 numbers = 5,6,4,5,1,2,-5
 print(sorted(numbers,reverse=True))
 print(sorted(numbers))
-<<<<<<< HEAD
-=======
+
 
 print("Reverse "+str(sorted(numbers,reverse=True)))
->>>>>>> e2c80fd7cbb5e880a623a3f0c58e6f0e92a65695
+
